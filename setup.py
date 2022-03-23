42b82609d4f31dd98190eccb2c84d4905bb92e48
ui906879 <Bear11112222>
2022-03-23 09:31:50 +0000

    add xlrd as requirement

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="excel_scraper",
-    version="0.0.4",
+    version="0.0.5",
     author="aeorxc",
     description="Utility library to scrape timeseries data from excel files into a pandas dataframe",
     url="https://github.com/aeorxc/excel-scraper",
@@ -15,7 +15,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=["pandas", "openpyxl"],
+    install_requires=["pandas", "openpyxl", "xlrd"],
     python_requires=">=3.8",
     setup_requires=["pytest-runner"],
     tests_require=["pytest"],
